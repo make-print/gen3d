9c24ade07e57526d333c1a223259557b90ee23da
Technocracy <erud1t3.devs@gmail.com>
2024-05-26 18:21:19 -0400

    fixed inference

--- sources/inference/hf_test_inference.py
+++ sources/inference/hf_test_inference.py
@@ -1,4 +1,10 @@
+import requests
+from PIL import Image
 from transformers import AutoProcessor, PaliGemmaForConditionalGeneration
+from huggingface_hub import login
+
+# Login to Hugging Face Hub
+login(token="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")
 
 model_id = "google/paligemma-3b-mix-224"
 model = PaliGemmaForConditionalGeneration.from_pretrained(model_id)
@@ -7,8 +13,7 @@ processor = AutoProcessor.from_pretrained(model_id)
 prompt = "What is on the flower?"
 image_file = "https://huggingface.co/datasets/huggingface/documentation-images/resolve/main/bee.jpg?download=true"
 raw_image = Image.open(requests.get(image_file, stream=True).raw)
-inputs = processor(prompt, raw_image, return_tensors="pt")
+inputs = processor(text=prompt, images=raw_image, return_tensors="pt")
 output = model.generate(**inputs, max_new_tokens=20)
 
 print(processor.decode(output[0], skip_special_tokens=True)[len(prompt):])
-# bee
